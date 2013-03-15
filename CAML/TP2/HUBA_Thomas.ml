9807bcfba88490cdd30f78f5d4f08c5cb5121185
ejeFi2in3F <thomas.huba@gmail.com>
2013-03-15 02:57:26 +0100

    correction

--- CAML/TP2/HUBA_Thomas.ml
+++ CAML/TP2/HUBA_Thomas.ml
@@ -27,7 +27,7 @@ let suite = function cons(x,y) -> y;
 
 (* Question 1 : Lire un fichier et faire la somme des entiers qui le composent *)
 
-let rec convert = function l -> 
+let convert = function l -> 
 	if l = vide then 0 else
 	cons(int_of_string(tete(l)),int_of_string(suite(l));;
 
