d385d051e34ce98c3605a59b8c4b62e2e292effc
5C5 <colas.picard.etu@univ-lemans.fr>
2013-03-29 15:09:11 +0100

    Mise a jour de la version suisse

--- octave/statdesc.m
+++ octave/statdesc.m
@@ -1,6 +1,7 @@
 %Picard Colas ----- L2SPI ----- TP5-----
 %page 63 du polycopié.
 
+
 function [moyenne, ecart, mediane, Q1, Q3] = statdesc (x)
 moyenne = 0;
 ecart = 0;
@@ -8,15 +9,23 @@ mediane = 0;
 Q1 = 0;
 Q3 = 0;
 if(nargin !=1)
-    usage ("statdesc (vector)");
+   usage ("statdesc (vector)")
 endif
-if(isvector (x))
+if(isvector(x))
     n = length(x);
     moyenne = sum(x)/ n;
-    ecart = sqrt(var(x));
+    ecart = sqrt(sum(x.^2 - moyenne^2)/n);
+    y = sort(x);
     mediane = median(x);
-    Q1 = median ([x(1):x(n/2)]);
-    Q3 = median ([x(n/2 + 1): x(n)]);
+    if(mod (n, 2) == 0)
+        w = y(1: N/2);
+        z = y(N/(2+1) :N);
+    else
+        w = y(1: floor(n/2));
+        z = y(floor(n/2) + 2:n);
+    endif
+    Q1 = median (w);
+    Q3 = median (z);
 else
     error ("statdesc : expecting vector argument");
 endif
