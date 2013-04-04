fa6c5e9875d30e796c9190376da100d46327660b
5C5 <colas.p.s@gmail.com>
2013-04-04 15:32:01 +0300

    Creation du tp4, version suisse

---
+++ CAML/tp4.ml
@@ -0,0 +1,25 @@
+(*Picard Colas --- Groupe TP5 ---- TP N°4*)
+type 'a liste = vide | cons of 'a * 'a liste;;
+type 'a arbre = V | N of 'a * 'a arbre * 'a arbre;;
+
+let tete = function cons(x, y) -> x
+  | vide -> failwith "impossible";;
+
+let suite = function cons(x, y) -> y
+	| vide -> failwith "impossible";;
+
+(*Question 1*)
+(*méthode deux*)
+let rec total_vendu_un = function l -> if l = vide then 0
+	else fst(tete l) + total_vendu_un(suite l);;
+
+(*méthode trois*)
+
+let rec total_vendu_deux = fun A V-> 0
+	|A(N(el, ag, ad)) -> fst(el) + total_vendu_deux (ag) + total_vendu_deux (ad);;
+
+(*Question 2*)
+
+(*Méthode Une*)
+
+let ajouter_vente_un = function (x , l) ->
