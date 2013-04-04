2d73a1edf7c3bc4ec412077ba99bf378dd8c2e1c
5C5 <colas.p.s@gmail.com>
2013-04-04 16:09:35 +0300

    Mise à jour de la version Suisse

--- CAML/tp4.ml
+++ CAML/tp4.ml
@@ -3,10 +3,14 @@ type 'a liste = vide | cons of 'a * 'a liste;;
 type 'a arbre = V | N of 'a * 'a arbre * 'a arbre;;
 
 let tete = function cons(x, y) -> x
-  | vide -> failwith "impossible";;
+	| vide -> failwith "impossible";;
 
 let suite = function cons(x, y) -> y
 	| vide -> failwith "impossible";;
+
+let rec inserer = fun element l -> if l = vide then cons (element, vide)
+	else if element < tete l then cons (element, l)
+		else cons(tete l, inserer element (suite l));;
 
 (*Question 1*)
 (*méthode deux*)
@@ -15,11 +19,15 @@ let rec total_vendu_un = function l -> if l = vide then 0
 
 (*méthode trois*)
 
-let rec total_vendu_deux = fun A V-> 0
-	|A(N(el, ag, ad)) -> fst(el) + total_vendu_deux (ag) + total_vendu_deux (ad);;
+let rec total_vendu_deux = function V-> 0
+	|N(el, ag, ad) -> fst(el) + total_vendu_deux ag + total_vendu_deux ad;;
 
 (*Question 2*)
 
 (*Méthode Une*)
 
-let ajouter_vente_un = function (x , l) ->
+let ajouter_vente_un = function (x , l) -> cons(x, l);;
+
+(*Méthode Deux*)
+
+let ajouter_vente_deux = function (x, l) -> inserer x l;;
