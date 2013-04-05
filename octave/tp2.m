35da1c1412d12806586bd1d9ee071872cd03e6aa
5C5 <colas.picard.etu@univ-lemans.fr>
2013-04-05 15:40:52 +0200

    mise à jour de la version suisse

--- octave/tp2.m
+++ octave/tp2.m
@@ -1,14 +1,27 @@
-m = 2^31;
-a = 843314861;
-b = 453816693;
-y = ones(101, 1);
-for i = 1 : 100
-    y(i +1) = mod(a * y(i) + b, m);
-endfor
-x = y/m;
-u = x(1 : 100);
-v = x(2 :101);
-plot(u, v ,'*')
-disp(x(1))
-disp(x(101))
-
+clear all;
+close all;
+m = 101;
+N = 100;
+%m = 2^31;
+%a = 843314861;
+% a = 12
+%b = 453816693;
+% b = 0;
+%y = ones(N, 1);
+%for i = 1 : N-1
+%    y(i +1) = mod(a * y(i) + b, m);
+%endfor
+%x = y/m;  
+%u = x(1 : 100);
+%v = x(2 :101);
+%plot(u, v ,'*')
+%disp(x(1))
+%disp(x(101))
+%r =x(1 : N-2, 1);
+%s = x (2 :N-1, 1);
+%t = x(3 : N, 1);
+%scatter3(r, s, t)
+x = rand(N, 1);
+u = x(1 : N-1);
+v = x(2 :N);
+plot(u, v, '*')
