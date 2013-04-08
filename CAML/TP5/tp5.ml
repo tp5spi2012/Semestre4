cf8fd66f9ca89da9da475aeb30ab02da6ea2bee2
5C5 <colas.picard.etu@univ-lemans.fr>
2013-04-08 14:19:07 +0200

    Mise a jour de la version suisse, avec ajout des fonctions vues en cours le 08/04

--- CAML/TP5/tp5.ml
+++ CAML/TP5/tp5.ml
@@ -1,3 +1,62 @@
 (*Colas PICARD ---- Groupe de TP1 ---- TP N° 5*)
 
 
+(*Déclaration de fonctions de lecture de fichier *)
+
+let is_sep = function ' ' | '\n' -> true
+    | _ -> false;;
+
+let is_sepu = function '+' | '-' | '*' | '(' | ')' -> true
+    | _  -> false;;
+
+let lire_car =function nom ->
+    let canal=open_in nom in
+        let rec lirea =function  () -> 
+        try let a = input_char canal in 
+            cons (a ,lirea ())  with
+    End_of_file -> begin close_in canal;vide end
+    in  lirea ();;
+
+let rec saute_terme = function vide -> vide
+    | cons(car, reste) as l ->
+
+let rec analyselex = function vide->vide
+    |cons (car,reste) as l -> if is_sep car then
+    analyselex reste
+        else if is_sepu car then
+            cons(char2str car, analyselex reste)
+            else cons(premier_terme l , analyselex reste (saute_terme l);;
+
+type terme = po|pf|op of string| entier of int;;
+
+let ch2terme = function
+    "(" -> po
+    |")" -> pf
+    |("+"|"-"|"*"|"/") as ope -> op ope
+    |v ->entier (int_of_string v);;
+
+let rec trans = fun f vide -> vide
+    | f (cons(tete, reste)) -> cons((f tete), (trans f reste));;
+
+let calculer_ebp = function ebp -> calculer ebp vide;;
+
+let rec calculer = fun vide (cons(entier v, vide)) -> v
+    | (cons(po, reste)) pile -> calculer reste pile
+    | (cons(pf, reste)) pile -> calculer reste (reduire pile)
+    | (cons(t, reste)) pile -> calculer reste (cons(t, pile))
+    | _ _ -> failwith "erreur calculer" ;;
+
+let reduire = function cons(t2, cons (ope cons( t1, reste))) -> cons(valeur ope t1 t2, reste)
+    |_ -> failwith "erreur";;
+
+let valeur = fun (op o as ope) (entier v1) (entier v2) ->
+    entier ((op2fun ope) v1 v2)
+    | _ _ _ -> failwith "erreur";;
+
+let op2fun = function op "+" -> prefix +
+    |op "-" -> prefix -
+    |op "*" -> prefix *
+    |op "/" -> prefix /
+    | _ -> failwith "impossible";;
+
+
